090c091e3809912397a001ca86cf24fabcb38e68
mmdii <mohammad.mohammadii1997@gmail.com>
2023-07-20 12:04:43 +0330

    add time code to pythons tests

--- pybind11/test.py
+++ pybind11/test.py
@@ -1,4 +1,14 @@
 import prime
+import time
+
+
+start_time = time.time()
 
 print(prime.is_prime(7))  # True
 print(prime.is_prime(12))  # False
+
+end_time = time.time()
+
+time_taken = end_time - start_time
+
+print("Time taken:", time_taken, "seconds")
